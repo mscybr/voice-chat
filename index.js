1475af5245b723415e3b940d9837c4862e310f58
Mo <52931433+mscybr@users.noreply.github.com>
2023-08-09 03:14:30 +0200

    Update index.js
    new

--- index.js
+++ index.js
@@ -1,19 +1,90 @@
-pth=require("path");
-require('app-module-path').addPath(__dirname);
-require("colors");
+const express = require("express");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost",
+    methods: ["GET", "POST"],
+  },
+});
 
-express=require("express");
-socketio=require("socket.io");
-path=require("path");
-ssocket=require("socket.io-stream");
-logger=require("core/log");
-os=require("os");
-https=require("https");
-fs=require("fs");
+//To hold user's information
+const socketsStatus = {};
+const sockets = {};
+let adminsocket = null;
+let state_of_mic = {};
 
-const l=new logger("main",os.hostname());
-const log=global.mainlog=l.log;
+http.listen(3000, () => {
+  console.log("the app is run in port 3000!");
+});
 
-log("Init");
-require("core/express");
-require("core/socket.io");
+io.on("connection", async function (socket) {
+  const socketId = socket.id;
+  socketsStatus[socketId] = { mute: true };
+  sockets[socketId] = socket;
+
+  socket.on("admin", function () {
+    adminsocket = socket;
+    delete socketsStatus[socketId];
+  });
+
+  socket.on("voice", function (data) {
+    var newData = data.split(";");
+    newData[0] = "data:audio/ogg;";
+    newData = newData[0] + newData[1];
+
+    if (adminsocket) adminsocket.emit("send", newData);
+    // for (const id in socketsStatus) {
+    //   if (id != socketId && !socketsStatus[id].mute) {
+    //     // socket.broadcast.to(id).emit("send", newData);
+    //   }
+    // }
+  });
+
+  socket.on("name", function (data) {
+    socketsStatus[socketId].name = data;
+    if (state_of_mic[data] == false) {
+      sockets[socketId].emit("unmute");
+      if (socketsStatus[socketId].mute) {
+        delete socketsStatus[socketId].mute;
+      }
+    }
+
+    console.log(state_of_mic);
+  });
+
+  socket.on("muteUser", function (data) {
+    socketsStatus[data].mute = true;
+    state_of_mic[socketsStatus[data].name] = true;
+
+    sockets[data].emit("mute");
+    refresh();
+    // io.sockets.emit("usersUpdate",socketsStatus);
+  });
+  socket.on("unmuteUser", function (data) {
+    delete socketsStatus[data].mute;
+    sockets[data].emit("unmute");
+    state_of_mic[socketsStatus[data].name] = false;
+
+    refresh();
+    // io.sockets.emit("usersUpdate",socketsStatus);
+  });
+
+  socket.on("getStatus", function () {
+    refresh();
+  });
+
+  socket.on("disconnect", function () {
+    delete socketsStatus[socketId];
+    delete sockets[socketId];
+    refresh();
+  });
+
+  function refresh() {
+    if (adminsocket) adminsocket.emit("refreshed_online", socketsStatus);
+  }
+
+  setTimeout(() => refresh(), 1000);
+});
